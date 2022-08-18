dda69543372061a6d4623fefbc4fa444127ee92a
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-08-18 17:16:53 -0300

    Initial commit

---
+++ app/src/main/java/com/wagarcdev/uitestxml/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wagarcdev.uitestxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
